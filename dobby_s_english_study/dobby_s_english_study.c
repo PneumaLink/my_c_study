aa73528949c0f63ee197a688f012a8e629b741b3
PneumaLink <chfhr0202@gmail.com>
2020-09-03 23:53:28 +0900

    dobbys_english_study.c

---
+++ dobby_s_english_study/dobby_s_english_study.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char	str[251];
+
+void	ft_gets(){
+	char	c;
+	int	idx;
+
+	idx = 0;
+	while (1){
+		c = fgetc(stdin);
+		if (c == '\n')
+			break;
+		str[idx++] = c;
+	}
+	str[idx] = '\0';
+}
+
+int	main(void){
+	int	idx;
+	int	line_num;
+	char	*arr_char;
+	int	*arr_int;
+
+	line_num = 0;
+	arr_char = (char*)malloc(sizeof(char));
+	arr_int = (int*)malloc(sizeof(int));
+ 	while (1){
+		ft_gets();
+		if (str[0] == '#')
+			break;
+		line_num++;
+		arr_char = (char*)realloc(arr_char, sizeof(char) * line_num);
+		arr_int = (int*)realloc(arr_int, sizeof(int) * line_num);
+		arr_char[line_num - 1] = str[0];
+		arr_int[line_num -1] = 0;
+		for (idx = 1 ; str[idx] != '\0' ; idx++){
+			if (arr_char[line_num - 1] == str[idx] || arr_char[line_num - 1] == str[idx] + ('a' - 'A')){
+				arr_int[line_num - 1]++;
+			}
+		}
+	}
+	for (idx = 0 ; idx < line_num ; idx++)
+		printf("%c %d\n", arr_char[idx], arr_int[idx]);
+	return (0);
+}
---
+++ test_dir/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int	main(){
+	printf("%lf", 0.5 * 0.5 * 0.5);
+	return (0);
+}
